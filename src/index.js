2c44ab0f522494ab0fd2135199afe11af5bd3175
shoogkabiya <shoogkabiya@hotmail.com>
2022-11-20 12:51:35 +0200

    adding some changing

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ var LastName = document.getElementById("LastName");
 var form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  // FirstName = FirstName.value;
-  // console.log("FirstName:", FirstName);
-  // LastName = LastName.value;
-  // console.log("LastName:", LastName);
-
   console.log("new data");
-  // <a href="Links.js" />;
   window.location.href = "https://github.com/shoogkabiya";
 });
