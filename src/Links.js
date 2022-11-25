625c5ef5f8009ea6b75b6472d1f23068e5696e1b
shoogkabiya <shoogkabiya@hotmail.com>
2022-11-25 16:11:49 +0200

    editing the object

--- src/Links.js
+++ src/Links.js
@@ -1,14 +1,16 @@
-const github = document.querySelector(".github");
+const github = document.createElement("img");
 const links = document.querySelector(".links");
-const linkedin = document.querySelector(".linkedin");
-const cv = document.querySelector(".cv");
+const linkedin = document.createElement("img");
+const cv = document.createElement("img");
 
 function displayImage() {
   github.src = "../images/github.webp";
-  // links.appendChild(github);
+  links.appendChild(github);
   linkedin.src = "../images/linkedin.png";
-  // links.appendChild(linkedin);
+  links.appendChild(linkedin);
   cv.src = "../images/cv.png";
-  // links.appendChild(cv);
+  links.appendChild(cv);
+  imageshoog.src = "../images/shoog.jpg";
 }
+
 displayImage();
