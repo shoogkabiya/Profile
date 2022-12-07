88fb49f00c0949da7f0579e5f35a77e5fff8d32e
shoogkabiya <shoogkabiya@hotmail.com>
2022-12-07 11:12:05 +0200

    Working  on move to cv bu clicking on image cv

--- src/Links.js
+++ src/Links.js
@@ -7,20 +7,23 @@ function displayImage() {
   github.src = "../images/github.webp";
   github.classList.add("github");
   links.appendChild(github);
-  github.onclick = function (event) {
+  github.onclick = function () {
     window.location.href = "https://github.com/shoogkabiya";
   };
 
   linkedin.src = "../images/linkedin.png";
   linkedin.classList.add("linkedin");
   links.appendChild(linkedin);
-  linkedin.onclick = function (event) {
+  linkedin.onclick = function () {
     window.location.href = "https://www.linkedin.com/in/shoog-kabiya/";
   };
 
   cv.src = "../images/cv.png";
   cv.classList.add("cv");
   links.appendChild(cv);
+  cv.onclick = function () {
+    window.location.href = "../images/shoogkabiya.pdf";
+  };
 }
 
 displayImage();
